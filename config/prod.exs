9e4c18b85137c84e3985210056b20307db4a4dbd
robert <robertschmidt4th@gmail.com>
2024-09-10 19:45:35 +0200

    ahhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh

--- config/prod.exs
+++ config/prod.exs
@@ -10,8 +10,8 @@ config :tickets2, Tickets2Web.Endpoint, cache_static_manifest: "priv/static/cach
 config :tickets2, Tickets2Web.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json",
   http: [
-    port: String.to_integer(System.get_env("PORT") || "4000"),
-    transport_options: [socket_opts: [:inet6]]
+    port: String.to_integer(System.get_env("PORT") || "4000")
+    # transport_options: [socket_opts: [:inet6]]
   ],
   url: [host: System.get_env("HOST") || "example.com", port: 80],
   secret_key_base: System.get_env("SECRET_KEY_BASE") || raise("SECRET_KEY_BASE is missing")
